260bf8776c07a0b9105d9bbf4dbc5799d6b61cca
bytelisa <elisa.marzioli2002@gmail.com>
2025-07-01 16:38:46 +0200

    ereditarietà: priority request specializza request

--- src/model/request.py
+++ src/model/request.py
@@ -13,14 +13,25 @@ class Request:
         req_type (RequestType, optional): Tipo di richiesta (es. checkout, ricerca).
                                           Può essere utile per analisi più dettagliate.
         arrival_time (float): Tempo di simulazione in cui la richiesta arriva.
+
+    """
+    request_id: int
+    req_type: RequestType
+    arrival_time: float
+
+
+
+#--- CLASSE DERIVATA (PER IL MIGLIORAMENTO) ---
+# Usiamo l'ereditarietà. PriorityRequest ha tutti i campi di Request più i suoi campi specifici.
+@dataclass
+class PriorityRequest(Request):
+    """
+    Rappresenta una richiesta specializzata con priorità e tempo di servizio.
+    Eredita da Request e aggiunge:
         priority (Priority): Classe di priorità della richiesta (es. ALTA, MEDIA, BASSA).
                              Determina da quale coda verrà servita.
         service_time (float): Tempo necessario a un Pod per processare questa specifica
                               richiesta. Dipende dal tipo/priorità.
     """
-    request_id: int
-    req_type: RequestType
-    arrival_time: float
     priority: Priority
-    service_time: float  # con le code di priorità diventa specifico della classe!
-
+    service_time: float
